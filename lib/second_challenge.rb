83a2d10f7787fcb6e0c046a6d378ff55f3ab0c06
Brian Salemi <briandsalemi@gmail.com>
2019-04-30 17:40:14 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriach => {},
-      :matriach => {},
-      :hero => {},
+      :patriach => {:name => "Lord Montague", :age => "53"},
+      :matriach => {:name => "Lady Montague", :age => "54"},
+      :hero => {:name => "Romeo", :age => "15", :status => "alive"},
       :hero_friends => []
       },
     :capulet => {
-      :patriach => {},
-      :matriach => {},
-      :heroine => {},
+      :patriach => {:name => "Lord Capulet", :age => "50"},
+      :matriach => {:name => "Lady Capulet", :age => "51"},
+      :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
       :heroine_friends => []
       }}
 end
